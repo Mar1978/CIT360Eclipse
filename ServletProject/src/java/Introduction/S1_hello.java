c24b4b49661a9788e3d2fd098abf6cc58eac82ed
Mar, Mike <31870347+Mikegmar@users.noreply.github.com>
2018-06-28 11:45:00 -0700

    added New Servlet

--- ServletProject/src/java/Introduction/S1_hello.java
+++ ServletProject/src/java/Introduction/S1_hello.java
@@ -3,12 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Serv01;
+package Introduction;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,10 +16,17 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author mikeg
  */
-@WebServlet(name = "FirstServlet", urlPatterns = {"/FirstServlet"})
-public class FirstServlet extends HttpServlet {
+public class S1_hello extends HttpServlet {
 
-
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -29,23 +35,38 @@ public class FirstServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet FirstServlet</title>");
+            out.println("<title>Servlet S1_hello</title>");            
             out.println("</head>");
-            out.println("<body>");                        
-            out.println("<h1> Hey, how are you? (Servlet) </h1>");
+            out.println("<body>");
+            out.println("<h1>Servlet S1_hello at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
             out.println("</html>");
         }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
- 
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
